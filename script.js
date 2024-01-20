fff77c64897e6fd71aef47d2544df8e3d6adbf2f
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-01-19 21:36:39 -0500

    layout to veritcal fixed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+const btnPrevious = document.querySelector(".btn btn--previous");
+const btnNext = document.querySelector(".btn btn--next");
+const instrumentEl = document.querySelector(".instrument");
+
+// document.getElementById(".btn btn-Previous").style.visibility = "hidden";
+
+// adding next picture button functionality
+btnNext.addEventListener("click", function () {
+  //   if ((instrumentEl.src = "instrument-1.jpeg")) {
+  //     instrumentEl.src = "instrument-2.jpeg";
+  //   }
+  // instrumentEl.src = `instrument-${dice}.jpeg`;
+});
