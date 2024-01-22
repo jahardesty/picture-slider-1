44f68a24cb7696950a01ccacca1f99e3ea592109
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-01-22 17:26:16 -0500

    timed slider working

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const instrumentEl = document.querySelector(".instrument");
 const checkBox = document.querySelector(".test");
 
 //adding next picture button functionality
+
+let i = 0; //start point
+let images = [];
+let time = 5000;
+
+// image list
+images[0] = "instrument-1.jpeg";
+images[1] = "instrument-2.jpeg";
+images[2] = "instrument-3.jpeg";
+images[3] = "instrument-4.jpeg";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
