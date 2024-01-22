2b219d997ae280713cd0c2c8a818e8207bdad090
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-01-22 14:53:53 -0500

    horizontally align buttons

--- script.js
+++ script.js
@@ -2,13 +2,6 @@
 const btnPrevious = document.querySelector(".btn btn--previous");
 const btnNext = document.querySelector(".btn btn--next");
 const instrumentEl = document.querySelector(".instrument");
+const checkBox = document.querySelector(".test");
 
-// document.getElementById(".btn btn-Previous").style.visibility = "hidden";
-
-// adding next picture button functionality
-btnNext.addEventListener("click", function () {
-  //   if ((instrumentEl.src = "instrument-1.jpeg")) {
-  //     instrumentEl.src = "instrument-2.jpeg";
-  //   }
-  // instrumentEl.src = `instrument-${dice}.jpeg`;
-});
+//adding next picture button functionality
